5aff25bd204a6e5a8c9f52e0dcc201346f26fbef
Alla Kobyzeva <apkobyzeva@yandex.ru>
2021-06-20 00:01:07 +0300

    v1

---
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+public class CommentsInfo {
+    private int count;
+    private boolean canPost;
+    private String commentsContent;
+    private String userId;
+    private int data;
+    private int time;
+    private LikesInfo likeInfo;
+    // + get/set на все поля
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+public class Post {
+    private String postId;
+    private String postUrl;
+    private String ownerId;
+    private int data;
+    private int time;
+    private String content;
+    private LikesInfo likeInfo;
+    private CommentsInfo commentsInfo;
+    private RepostsInfo repostInfo;
+    private ViewsInfo viewInfo;
+}
+// + get/set на все поля
+
+
